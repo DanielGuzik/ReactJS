c6d3c73e75279d6ca399c83411d17cd205f82141
Daniel <Danielguzik6@gmail.com>
2018-11-28 00:16:40 +0100

    Rozdział 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,22 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-	return <div> Daniel Guzik </div>;
+	const labelText = 'Enter name';
+	// const buttonText = 'Wyślij!';
+	const buttonText = { text: 'Submit' };
+	const styleButton = { backgroundColor: 'blue', color: 'white' };
+
+	return (
+		<div>
+			<label className="label" htmlFor="name"> {labelText} </label>
+			<input id="name" type="text" />
+			<button style={styleButton}> {buttonText.text} </button>
+		</div>
+	);
 };
 
 // Take the react component and show to the screen
-ReactDOM.render(<App/>,	document.querySelector("#root"));
+ReactDOM.render(
+	<App />,
+	document.querySelector("#root")
+);
